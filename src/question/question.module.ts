40c32b06a23ab44e28cae30cf45c723d0579410e
KHJcode <kimstudswcode4485@gmail.com>
2021-08-03 11:54:27 +0900

    add question api

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Question } from './entity/question.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Question])],
   providers: [QuestionService],
-  controllers: [QuestionController]
+  controllers: [QuestionController],
 })
 export class QuestionModule {}
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Question } from './entity/question.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+  constructor(
+    @InjectRepository(Question)
+    private questionRepository: Repository<Question>,
+  ) {}
+
+  async findTodayQuestionByType(type: string): Promise<Question> {
+    return await this.questionRepository.findOne({
+      where: { type },
+      order: { id: 'DESC' },
+    });
+  }
+}
